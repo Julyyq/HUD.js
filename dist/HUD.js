dfc72e18b90d814dd6535a13c47804f7ce845a12
Julyyq <colorfuljuly@gmail.com>
2015-06-16 18:14:09 +0800

    fix HUD position if scroll bar is not on the top

--- dist/HUD.js
+++ dist/HUD.js
@@ -1,7 +1,7 @@
 ﻿;(function() {
 	var defaultConfig = {
 		"tplMask": "<div id='HUD-mask' style='visibility:hidden; position: fixed; width: 100%; height: 100%; left: 0; top: 0; background-color: black; opacity: 0.6; z-index: 999999999;'></div>",
-		"tplHUD": "<div id='HUD' style='visibility:hidden; position: absolute; left: 50%; top: 50%; background-color: #FF5656; color: white; padding: 5px 10px; font-size: 14px; -webkit-font-smoothing: antialiased; border-radius: 5px; text-align: center; z-index: 999999999;'><canvas id='HUD-canvas' width='100' height='100'></canvas><p><%= HUD.text %></p></div>",
+		"tplHUD": "<div id='HUD' style='visibility:hidden; position: fixed; left: 50%; top: 50%; background-color: #FF5656; color: white; padding: 5px 10px; font-size: 14px; -webkit-font-smoothing: antialiased; border-radius: 5px; text-align: center; z-index: 999999999;'><canvas id='HUD-canvas' width='100' height='100'></canvas><p><%= HUD.text %></p></div>",
 		"imageColor": "#FFFFFF",
 		"text": "loading..."
 	}
